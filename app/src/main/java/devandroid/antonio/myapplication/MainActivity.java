590c4e3d7ce04af9cef36616dadfa34ed765f4f4
Antonio <antonio-emilio_6@hotmail.com>
2023-05-10 18:28:20 -0300

    Tela Inicial

--- app/src/main/java/devandroid/antonio/myapplication/MainActivity.java
+++ app/src/main/java/devandroid/antonio/myapplication/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnRegister.setOnClickListener(view -> {
 
-            Intent intent = new Intent(this,Registro.class);
+            Intent intent = new Intent(this,TelaInicial.class);
             startActivity(intent);
 
         });
