c0ae93b1cd2eb28ff324256b66f8878611cc8b29
MatheusBeneti <matheusbeneti3@gmail.com>
2023-05-22 11:30:31 -0300

    adicionei a lógica para implementar ao clique de cada botão do menu

--- app/src/main/java/devandroid/antonio/myapplication/TelaInicial.java
+++ app/src/main/java/devandroid/antonio/myapplication/TelaInicial.java
@@ -1,6 +1,8 @@
 package devandroid.antonio.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomappbar.BottomAppBar;
@@ -12,5 +14,22 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+    }
+    public void buttonMapas(MenuItem item) {
+        // Lógica para tratar o clique no item "COMPS"
+        Intent intent = new Intent();
+        startActivity(intent);
+    }
+
+    public void buttonPersonagens(MenuItem item) {
+        // Lógica para tratar o clique no item "PERSONAGENS"
+        Intent intent = new Intent();
+        startActivity(intent);
+    }
+
+    public void buttonComps(MenuItem item) {
+        // Lógica para tratar o clique no item "COMPS"
+        Intent intent = new Intent();
+        startActivity(intent);
     }
 }
