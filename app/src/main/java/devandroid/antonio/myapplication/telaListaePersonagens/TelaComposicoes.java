2d99c93c26345ff3b85528afc28221dd2bba438d
Beneti <matheusbeneti2@gmail.com>
2023-06-04 12:59:32 -0300

    banco implementado

--- app/src/main/java/devandroid/antonio/myapplication/telaListaePersonagens/TelaComposicoes.java
+++ app/src/main/java/devandroid/antonio/myapplication/telaListaePersonagens/TelaComposicoes.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.os.strictmode.SqliteObjectLeakedViolation;
 import android.widget.ArrayAdapter;
@@ -26,6 +27,7 @@ public class TelaComposicoes extends AppCompatActivity {
         listViewDados = (ListView) findViewById(R.id.listViewDados);
 
         criarBancoDados();
+        inserirDadosTemp();
         listarDados();
     }
 
@@ -35,6 +37,7 @@ public class TelaComposicoes extends AppCompatActivity {
             bancoDados.execSQL("CREATE TABLE IF NOT EXISTS comps("+
                     "id INT PRIMARY KEY,"+
                     "nome VARCHAR(30))");
+            bancoDados.close();
 
         }catch (Exception e){
             System.out.println("ERROR");
@@ -47,9 +50,36 @@ public class TelaComposicoes extends AppCompatActivity {
             Cursor meuCursor = bancoDados.rawQuery("SELECT id, nome FROM comps;",null);
             ArrayList<String> linhas = new ArrayList<String>();
             ArrayAdapter meuAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, linhas);
-        }catch (Exception e){
-
+            listViewDados.setAdapter(meuAdapter);
+            meuCursor.moveToFirst();
+            while(meuCursor != null){
+                linhas.add(meuCursor.getString(1));
+                meuCursor.moveToNext(); // Adicionar esta linha para mover para o próximo registro
+            }
+            bancoDados.close();
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
+    public void inserirDadosTemp(){
+        try {
+            bancoDados = openOrCreateDatabase("comps", MODE_PRIVATE, null);
+            String sql = "INSERT INTO comps (nome) VALUES (?)";
+            SQLiteStatement stmt = bancoDados.compileStatement(sql);
 
+            stmt.bindString(1, "coisa 1");
+            stmt.executeInsert();
+
+            stmt.bindString(1, "coisa abc");
+            stmt.executeInsert();
+
+            stmt.bindString(1, "coisa terceira");
+            stmt.executeInsert();
+
+            bancoDados.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
